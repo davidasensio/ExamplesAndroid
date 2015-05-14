1e0a9b08378cb8ee986089ba6c0edd9206c3622d
david <davidasensio81@gmail.com>
2015-05-14 18:58:26 +0200

    cambios

---
+++ fragments/src/main/java/com/curso/androidt/fragments/ListadoFragment.java
@@ -0,0 +1,35 @@
+package com.curso.androidt.fragments;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ListadoFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ListadoFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ListadoFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_listado, container, false); //False hace que no aplique las caracteristicas del padre
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
