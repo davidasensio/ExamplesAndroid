0fd9acaad1c59ec785084fe7281cf6a3abc5ba30
david <davidasensio81@gmail.com>
2015-05-15 16:28:44 +0200

    cambios

--- fragments/src/main/java/com/curso/androidt/fragments/MainActivity.java
+++ fragments/src/main/java/com/curso/androidt/fragments/MainActivity.java
@@ -4,17 +4,43 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends Activity  {
+
+    ListView listViewTareas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listViewTareas = (ListView) findViewById(R.id.listViewTareas);
+
+        List<Tarea> listTareas = new ArrayList<Tarea>();
+        listTareas.add(new Tarea("Tarea 1", "Esta es la tarea 1", 10, new Date()));
+        listTareas.add(new Tarea("Tarea 2", "Esta es la  tarea 2", 20, new Date()));
+        listTareas.add(new Tarea("Tarea 3", "Esta es la tarea 3", 30, new Date()));
+        listTareas.add(new Tarea("Tarea 4", "Esta es la tarea 4", 40, new Date()));
+
+        ListAdapter listTareaAdapter = new TareasAdapter(listTareas, R.layout.tarea_list_item, this);
+
+
+        listViewTareas.setAdapter(listTareaAdapter);
+        registerForContextMenu(listViewTareas);
+
     }
 
     @Override
@@ -39,17 +65,46 @@ public class MainActivity extends Activity implements View.OnClickListener {
         return super.onOptionsItemSelected(item);
     }
 
+    //Context Menu
+
     @Override
-    public void onClick(View v) {
-        //Discriminar si estamos en SmartPhone o en Tablet
-        Fragment fragmentDetalle = getFragmentManager().findFragmentById(R.id.fragmentDetalle1);
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
 
-        if (fragmentDetalle != null) {
+        if (v.getId() == R.id.listViewTareas) {
+            getMenuInflater().inflate(R.menu.menu_main, menu);
+            int position = ((AdapterView.AdapterContextMenuInfo) menuInfo).position;
+            Tarea tarea = (Tarea)listViewTareas.getAdapter().getItem(position);
 
-        }else {
-            Intent intent = new Intent(this, DetalleActivity.class);
-            startActivity(intent);
+            menu.setHeaderIcon(android.R.drawable.btn_star);
+            menu.setHeaderTitle(tarea.getTitulo());
         }
-
     }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_settings:
+                int position = ((AdapterView.AdapterContextMenuInfo) item.getMenuInfo()).position;
+                Tarea tarea = (Tarea)listViewTareas.getAdapter().getItem(position);
+
+                //Discriminar si estamos en SmartPhone o en Tablet
+                Fragment fragmentDetalle = getFragmentManager().findFragmentById(R.id.fragment3);
+
+                if (fragmentDetalle != null) {
+                   // fragmentDetalle.find
+
+                }else {
+                    Intent intent = new Intent(this, DetalleActivity.class);
+                    startActivity(intent);
+                }
+
+                Toast.makeText(this, "Item pulsado " + tarea.getTitulo(), Toast.LENGTH_LONG).show();
+                return true;
+        }
+        return super.onContextItemSelected(item);
+    }
+
+
 }
---
+++ fragments/src/main/java/com/curso/androidt/fragments/TareasAdapter.java
@@ -0,0 +1,73 @@
+package com.curso.androidt.fragments;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+/**
+ * Created by androidt on 13/05/2015.
+ */
+public class TareasAdapter extends BaseAdapter {
+
+
+    private List<Tarea> datos;
+    private int resLayout;
+    private Context context;
+
+    public TareasAdapter(List<Tarea> datos, int resLayout, Context context) {
+        this.datos = datos;
+        this.resLayout = resLayout;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return datos.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return getDatos().get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(resLayout, parent, false);
+        }
+
+        Tarea item = getDatos().get(position);
+
+        TextView txtTitulo = (TextView) convertView.findViewById(R.id.txtTitulo);
+        TextView txtDescripcion = (TextView) convertView.findViewById(R.id.txtDescripcion);
+        TextView txtPrioridad = (TextView) convertView.findViewById(R.id.txtPrioridad);
+        TextView txtFecha = (TextView) convertView.findViewById(R.id.txtFecha);
+
+        txtTitulo.setText(item.getTitulo());
+        txtDescripcion.setText(item.getDescripcion());
+        txtPrioridad.setText(String.valueOf(item.getPrioridad()));
+        txtFecha.setText(new SimpleDateFormat("dd/MM/yyyy").format(item.getFecha()));
+
+        return convertView;
+    }
+
+    public List<Tarea> getDatos() {
+        return datos;
+    }
+
+    public void setDatos(List<Tarea> datos) {
+        this.datos = datos;
+    }
+}
