31b6002880722ff2eb5acc8aebf9ac90471e8a69
david <davidasensio81@gmail.com>
2015-05-18 19:09:41 +0200

    cambios

---
+++ configuracionfragment/src/main/java/com/curso/androidt/configuracionfragment/ConfiguracionFragment.java
@@ -0,0 +1,34 @@
+package com.curso.androidt.configuracionfragment;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ConfiguracionFragment extends PreferenceFragment {
+
+
+    public ConfiguracionFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return null;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
